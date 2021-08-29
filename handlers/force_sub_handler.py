4c638cc20bb8835bf6f1da04c92c7047eb94b7eb
Tampan793 <69372834+Tampan793@users.noreply.github.com>
2021-08-30 02:47:13 +0700

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -47,7 +47,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/DevsZone).",
+            text="Something went Wrong. Contact my [Support Helps](https://t.me/xH4X0Rx).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
